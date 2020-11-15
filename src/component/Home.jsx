f03662db7ae3c02eadf898966a369368b18281da
AkhileshPandeyji <pandeyakhilesh5372@gmail.com>
2020-11-15 17:23:08 +0530

    Improved Card UI

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,27 +2,29 @@ import React, { useState } from 'react';
 import ProductCard from './ProductCard';
 
 const initialProducts = [
-    {name:"Apple Iphone 11 Pro",price:4500,img:"https://picsum.photos/200",rating:4.0},
-    {name:"Apple Iphone 11 Pro",price:4500,img:"https://picsum.photos/200",rating:4.0},
-    {name:"Apple Iphone 11 Pro",price:4500,img:"https://picsum.photos/200",rating:4.0},
-    {name:"Apple Iphone 11 Pro",price:4500,img:"https://picsum.photos/200",rating:4.0},
-    {name:"Apple Iphone 11 Pro",price:4500,img:"https://picsum.photos/200",rating:4.0},
-    {name:"Apple Iphone 11 Pro",price:4500,img:"https://picsum.photos/200",rating:4.0},
-    {name:"Apple Iphone 11 Pro",price:4500,img:"https://picsum.photos/200",rating:4.0},
-    {name:"Apple Iphone 11 Pro",price:4500,img:"https://picsum.photos/200",rating:4.0},
-    {name:"Apple Iphone 11 Pro",price:4500,img:"https://picsum.photos/200",rating:4.0},
-    {name:"Apple Iphone 11 Pro",price:4500,img:"https://picsum.photos/200",rating:4.0}
+    {name:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, est?",price:4500,img:"https://picsum.photos/id/0/200/300",rating:4.0},
+    {name:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, est?",price:4500,img:"https://picsum.photos/id/0/200/300",rating:4.0},
+    {name:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, est?",price:4500,img:"https://picsum.photos/id/0/200/300",rating:4.0},
+    {name:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, est?",price:4500,img:"https://picsum.photos/id/0/200/300",rating:4.0},
+    {name:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, est?",price:4500,img:"https://picsum.photos/id/0/200/300",rating:4.0},
+    {name:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, est?",price:4500,img:"https://picsum.photos/id/0/200/300",rating:4.0},
+    {name:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, est?",price:4500,img:"https://picsum.photos/id/0/200/300",rating:4.0},
+    {name:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, est?",price:4500,img:"https://picsum.photos/id/0/200/300",rating:4.0},
+    {name:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, est?",price:4500,img:"https://picsum.photos/id/0/200/300",rating:4.0},
+    {name:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, est?",price:4500,img:"https://picsum.photos/id/0/200/300",rating:4.0}
 ];
 
 const Home = () => {
     const [products,changeProducts] = useState(initialProducts);
     return (
         <div className="home">
+            <div className="product-dashboard">
             {
                 products.map((product)=>{
                     return (<ProductCard name={product.name} price={product.price} img={product.img} rating={product.rating} />);
                 })
             }
+            </div>            
         </div>
     );
 }
--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 const ProductCard = (props) => {
     return (
         <div className="product-card">
-            <div className="product-title">{props.title}</div>
             <div className="product-image">
                 <img src={props.img}  alt="product-img" />
             </div>
-            <div className="product-price">{props.price}</div>
+            <div className="product-price red"><span><i class="fas fa-rupee-sign red"></i></span>{props.price}</div>
+            <div className="product-title"><a href="#">{props.name}</a></div>
             <div className="product-rating">{props.rating}</div>
+            <div className="product-add-btn"><button>Add To Cart</button></div>
         </div>
     );
 }
